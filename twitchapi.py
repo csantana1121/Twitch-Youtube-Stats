c8d93c24a4e2ab341d39eaa8adff4e353357c715
Christian Santana <christians1121@gmail.com>
2021-07-21 17:43:20 +0000

    almost there on integration with webui

--- twitchapi.py
+++ twitchapi.py
@@ -36,13 +36,23 @@ def get_user_query(user_login):
     return 'users?login={0}'.format(user_login)
 
 def get_user_videos_query(user_id):
-    return f'streams?login={0}&first=50'.format(user_id)
+    return 'videos?user_id={0}&first=50'.format(user_id)
 
 def test_method():
     user_login = 'gorgc'
 
     query = get_user_query(user_login)
     response = get_response(query)
-
-    print_response(response)
+    user_info = response.json()
+    user_id = user_info['data'][0]['id']
+    img_url = user_info['data'][0]['profile_image_url']
+    print(user_id)
+    print(img_url)
+    user_videos_query = get_user_videos_query(user_id)
+    videos_info = get_response(user_videos_query)
+    print(videos_info.status_code)        
+    videos_info_json = videos_info.json()
+    print(videos_info_json) 
+    
+    # print_response(response)
 
